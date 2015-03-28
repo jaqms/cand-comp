3180973bd72dc2a07db868e072d51e4ea234ad69
unknown <jaqms90@gmail.com>
2015-03-28 16:46:52 -0400

    Add handlebar and main javascript/css

---
+++ js/main.js
@@ -0,0 +1,105 @@
+function IssueCollection(json) {
+	this.issues = [];
+}
+
+IssueCollection.prototype.add = function (issue) {
+	this.issues.push(issue);
+}
+
+IssueCollection.prototype.findByName = function (name) {
+	var issueFind = this.issues.filter(function (c) {
+		return c.id === name;
+	});
+	
+	if (issueFind.length === 1)
+		return issueFind[0];
+	else
+		return null;
+}
+
+function Issue(json) {
+	this.id = json['gsx$name']
+	this.name = json['gsx$name'];
+	this.slug = json['gsx$slug'];
+	this.icon = json['gsx$icon'];
+	this.color = json['gsx$color'];
+}
+
+function CandidateCollection() {
+	this.candidates = [];
+}
+
+CandidateCollection.prototype.add = function (candidate) {
+	this.candidates.push(candidate);	
+}
+
+CandidateCollection.prototype.findByName = function (name) {
+	var candidateFind = this.candidates.filter(function (c) {
+		return c.id === name;
+	});
+	
+	if (candidateFind.length === 1)
+		return candidateFind[0];
+	else
+		return null;
+}
+
+function Candidate(id) {
+	this.id = id;
+	this.bioData = {};
+	this.issueData = {};
+}
+
+Candidate.prototype.setBioData = function (json) {
+	Object.keys(json).forEach(function (i) {
+		if (i.indexOf("gsx") === 0 && i !== 'gsx$name') {
+			this.bioData[i.substring(4)] = json[i]['$t'];
+		}
+	});
+}
+
+Candidate.prototype.setIssueData = function (json) {
+	Object.keys(json).forEach(function (i) {
+		if (i.indexOf("gsx") === 0 && i !== 'gsx$name') {
+			this.issueData[i.substring(4)] = json[i]['$t'];
+		}
+	});
+}
+
+
+function getGoogleSheet(id, callback) {
+	$.getJSON("https://spreadsheets.google.com/feeds/list/1y4BwQfdE0-isBz8VptmS0n_52TyLRv7BPLp5K5QHqho/" + id + "/public/values?alt=json",
+		function (data) {
+			callback(data)
+		};
+}
+
+window.onload = function () {
+	window.issues = new IssueCollection();
+	window.candidates = new CandidateCollection();
+	
+	getGoogleSheet(3, function (data) {
+		data['feed']['entry'].forEach(function (entry) {
+			var issue = new Issue(entry);
+			issueCollection.add(candidate);
+		});
+
+		getGoogleSheet(2, function (data) {
+			data['feed']['entry'].forEach(function (entry) {
+				var candidate = new Candidate(entry['gsx$name']['$t']);
+				candidate.setBioData(entry);
+				candidateCollection.add(candidate);
+			});
+			
+			getGoogleSheet(1, function (data) {
+				data['feed']['entry'].forEach(function (entry) {
+					var candidate = candidateCollection.findByName(entry['gsx$name']['$t']);
+					if (candidate) {
+						candidate.setIssueData(entry);
+					}
+				});
+			});
+		});
+	});
+
+}
